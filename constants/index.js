01f09ea533cedca97c4e4e8c39cb6ae328b857f7
arboldyrev <artemboldyrev@altitudedp.com>
2024-05-06 14:44:21 +0300

    +

---
+++ constants/index.js
@@ -0,0 +1,18 @@
+export const apiKey = '573a75ffaeaa4eacb58142519240604';
+
+export const weatherImages = {
+    'partly cloudy': require('../assets/images/partlycloudy.png'),
+    'Moderate rain ': require('../assets/images/moderaterain.png'),
+    'Patchy rain possible ': require('../assets/images/moderaterain.png'),
+    'Sunny': require('../assets/images/sun.png'),
+    'Clear': require('../assets/images/sun.png'),
+    'Overcast': require('../assets/images/cloud.png'),
+    'Cloudy': require('../assets/images/cloud.png'), 
+    'Light rain ': require('../assets/images/moderaterain.png'),
+    'Heavy rain ': require('../assets/images/heavyrain.png'),
+    'Heavy rain at times ': require('../assets/images/heavyrain.png'), 
+    'Moderate or heavy freezing rain ': require('../assets/images/heavyrain.png'),
+    'Moderate or heavy rain shower ': require('../assets/images/heavyrain.png'), 
+    'Moderate or heavy rain with thunder ': require('../assets/images/heavyrain.png'),
+    'other ': require('../assets/images/moderaterain.png'),
+} 
---
+++ navigation/appNavigation.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { LogBox, Text, View } from "react-native";
+import HomeScreen from "../screens/HomeScreen";
+
+const Stack = createStackNavigator();
+LogBox.ignoreLogs(['Non-serializable values were found in the navigation state']);
+
+const AppNavigation = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" options={{ headerShown: false }} component={HomeScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default AppNavigation;
